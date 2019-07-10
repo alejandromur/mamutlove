ac843fd1e009e439da1d9e60450af2fd545fb748
Alejandro Mur <alejandro.mur@boream.com>
2019-07-10 23:45:49 +0200

    localstorage set/get

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,6 +7,11 @@
 // }
 
 // Toggle Dark Theme
+if (localStorage.getItem('theme')) {
+  const theme = localStorage.getItem('theme');
+  document.body.classList.add(theme);
+}
+
 const toggleTheme = document.querySelector('.js-toggle-theme');
 
 toggleTheme.addEventListener('click', e => {
@@ -14,7 +19,9 @@ toggleTheme.addEventListener('click', e => {
 
   if (!document.body.classList.contains('theme-dark')) {
     document.body.classList.add('theme-dark');
+    localStorage.setItem('theme', 'theme-dark');
   } else {
     document.body.classList.remove('theme-dark');
+    localStorage.removeItem('theme');
   }
 });
